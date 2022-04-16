e20978e22b6d7f54e0d43ed2b80bbce208419a04
bellsilver7 <bellsilver7@gmail.com>
2022-04-16 21:08:24 +0900

    gatsby blog by react

---
+++ contents/Default.md
@@ -0,0 +1,15 @@
+---
+date: '2022-04-16'
+title: 'First'
+categories: ['Web', 'SEO', 'Optimization']
+summary: '테스트 작성 글입니다.'
+thumbnail: './Default.png'
+---
+
+### 제목
+
+- 리스트
+
+Kick off your project with this blog boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
+
+Have another more specific idea? You may want to check out our vibrant collection of official and community-created starters.
---
+++ src/pages/index.tsx
@@ -0,0 +1,97 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import Template from 'components/Common/Template';
+import styled from '@emotion/styled';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+import PostList from 'components/Main/PostList';
+import { graphql } from 'gatsby';
+import { PostListItemType } from 'types/PostItem.types';
+import queryString, { ParsedQuery } from 'query-string';
+
+type IndexPageProps = {
+  location: {
+    search: string;
+  };
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[];
+    };
+  };
+};
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  location: { search },
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const parsed: ParsedQuery<string> = queryString.parse(search);
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category;
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'],
+          {
+            node: {
+              frontmatter: { categories },
+            },
+          }: PostType,
+        ) => {
+          categories.forEach(category => {
+            if (list[category] === undefined) list[category] = 1;
+            else list[category]++;
+          });
+
+          list['All']++;
+
+          return list;
+        },
+        { All: 0 },
+      ),
+    [],
+  );
+
+  return (
+    <Template>
+      <Introduction />
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
+    </Template>
+  );
+};
+
+export default IndexPage;
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`;
